22ef0c3fe838803c4eb7913aafb35976c87e5612
ErikManarin <erik.bm.mana@gmail.com>
2024-03-14 15:34:37 -0300

    inicial

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SQLite from 'expo-sqlite';
 
-export default function App() {
+const db = SQLite.openDatabase('db.testDb'); // Abre uma base de dados chamada "testDb"
+
+export default function SQLiteScreen() {
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(
+        "create table if not exists items (id integer primary key not null, value text);",
+        [],
+        () => { console.log('Tabela criada com sucesso'); },
+        error => { console.log('Erro ao criar tabela: ' + error.message); }
+      );
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Bem-vindo ao seu app Expo com SQLite!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +30,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); 
---
+++ src/screens/ProdutosScreen.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { Image, View } from "react-native";
+import { Text } from "react-native-paper";
+import styles from "../config/styles";
+
+export default function ProdutosScreen() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(
+    () => {
+      fetchProducts();
+    },
+    [] 
+  );
+
+  const fetchProducts = async () => {
+
+    const response = await fetch("https://dummyjson.com/products");
+
+    const data = await response.json();
+
+
+    console.log(data.products);
+
+    setProdutos(await data.products);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/*note que aqui estamos usando o TEXT de Native Paper*/}
+      {/*ele possui alguns props que podemos usar para melhorar a UI*/}
+      <Text variant="titleLarge">Produtos</Text>
+      <Text variant="bodyMedium">Confira a lista de produtos</Text>
+
+      {
+        produtos.length === 0 ? (
+          <Text>Carregando...</Text>
+        ) : (
+         
+          produtos.map((produto) => (
+            <View key={produto.id} style={styles.selfStrech}>
+              <Text variant="headlineMedium" >{produto.title}</Text>
+              {/*exibindo a imagem do produto*/}
+              <Image
+               
+                source={{ uri: produto.images[0] }}
+               
+                style={{ width: "100%", maxWidth: 480, height: 320 }}
+              />
+            </View>
+          ))
+        )
+      }
+    </View>
+  );
+}
